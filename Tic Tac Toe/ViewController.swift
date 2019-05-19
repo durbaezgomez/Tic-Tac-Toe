6e402cc695bf8a61dcdd533c9e10ddefd026139a
Dominik <dominiug@yahoo.com>
2019-05-19 14:33:17 +0200

    wins for both the player and bot available
    - added functionality to check for either player's win
    - updated assets used to visualise a win

--- Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/ViewController.swift
@@ -10,37 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let rect_blank = UIImage(named: "rect_blank")
-    let rect_cross = UIImage(named: "rect_cross")
-    let rect_circle = UIImage(named: "rect_circle")
-    
+   
     @IBOutlet var rectangles: [UIButton]!
     
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    var activePlayer = 1
-    var isPlayersMove:Bool = true
+    var winSeq:[Int] = [0,0,0]
+    var winner:Int?
     
     @IBAction func restartGame(_ sender: UIButton) {
+        resetGameState()
+    }
+    
+    func resetGameState(){
         gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        winSeq = [0,0,0]
         for rect in rectangles{
-            rect.setImage(rect_blank, for: .normal)
+            rect.isHidden = false
+        }
+        for rect in rectangles{
+            rect.setImage(UIImage(named: "rect_blank"), for: .normal)
         }
     }
+    
     @IBAction func fieldChosen(_ sender: UIButton) {
         
-        if(gameState[sender.tag-1] == 0){
-            sender.setImage(rect_cross, for: .normal)
-            gameState[sender.tag-1] = activePlayer
-            
-            var count = 0
-            for i in gameState{
-                if (i != 0) {count += 1}
-            }
-            if (count < 9){
-                playNextMove()
-            }
-            print(gameState)
+        var count = 0
+        for i in gameState{
+            if (i != 0) {count += 1}
         }
+        
+        if(gameState[sender.tag-1] == 0){
+            sender.setImage(UIImage(named: "rect_cross"), for: .normal)
+            gameState[sender.tag-1] = 1
+            
+            lookForVictory()
+        }
+        
+        if (count > 7){
+            print("DRAW")
+        }
+        
+        else {
+            playNextMove()
+            print("YOUR MOVE")
+            lookForVictory()
+        }
+//        print(gameState)
     }
     
     func playNextMove(){
@@ -50,11 +65,88 @@ class ViewController: UIViewController {
             playedSpot = arc4random_uniform(9)
         }while(gameState[Int(playedSpot)] != 0)
         
-        rectangles[Int(playedSpot)].setImage(rect_circle, for: .normal)
-        gameState[Int(playedSpot)] = activePlayer
+        rectangles[Int(playedSpot)].setImage(UIImage(named: "rect_circle"), for: .normal)
+        gameState[Int(playedSpot)] = 2
     }
     
+    func lookForVictory(){
+        if (checkIfWon()){
+            print("WIN")
+            print(winSeq)
+            showVictory(winner: winner!)
+        }
+    }
     
+    func checkIfWon() -> Bool{
+        for p in 1...2{
+            var counter: Int
+            for i in 0...2{ // rows
+                counter = 0
+                for j in 0...2{
+                    if (gameState[j+i*3] == p){
+                        winSeq[j] = j+i*3
+                        counter += 1
+                    }
+                    if (counter == 3){
+                        winner = p
+                        return true
+                    }
+                }
+            }
+            for i in 0...2{ // columns
+                counter = 0
+                for j in 0...2{
+                    if (gameState[j*3+i] == p){
+                        winSeq[j] = j*3+i
+                        counter += 1
+                    }
+                    if (counter == 3){
+                        winner = p
+                        return true
+                    }
+                }
+            }
+            // diagonals
+            counter = 0
+            if (gameState[0] == p && gameState[4] == p && gameState[8] == p){
+                winSeq = [0,4,8]
+                winner = p
+                return true
+            }
+            if (gameState[2] == p && gameState[4] == p && gameState[6] == p){
+                winSeq = [2,4,6]
+                winner = p
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    func showVictory(winner: Int){
+        for rect in rectangles{
+            rect.setImage(UIImage(named: "rect_inactive"), for: .normal)
+        }
+        if (winner == 1){
+            for val in winSeq{
+                rectangles[val].setImage(UIImage(named: "rect_cross_win"), for: .normal)
+            }
+        }
+        else{
+            for val in winSeq{
+                rectangles[val].setImage(UIImage(named: "rect_circle_win"), for: .normal)
+            }
+        }
+        
+    }
+
+    func checkDanger(){
+        
+        //       TODO: SPRAWDZANIE ZAGROZEN -> ZWROC INDEX DO BLOKADY
+        //       TODO: SZUKANIE MOZLIWOSCI WYGRANEJ -> ZWROC INDEX WYGRANEJ
+        
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
